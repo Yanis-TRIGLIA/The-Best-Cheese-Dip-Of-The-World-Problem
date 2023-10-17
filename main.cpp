92f11298f9b37f5c4df19f1689a5f17b1ddb3a0e
Thibault-De-Permentier <91873613+Thibault-De-Permentier@users.noreply.github.com>
2023-10-17 21:12:38 +0200

    code related to the 3 stars swiss chef and thread

--- main.cpp
+++ main.cpp
@@ -24,11 +24,37 @@ void getHelp(unsigned i){
     kitchenAssistantsMutex.unlock();
 }
 
-void helpKitchenAssistants(){}
+void helpKitchenAssistants(){
+    kitchenAssistantsMutex.lock();
+    //curently helping kitchen assistants -> ToDO : remember to change waiting values
+    cout << "j'aide 3 assistant" << endl;
 
-void giveCheese(){}
+    chrono::microseconds val (experimental::fundamentals_v2::randint(0, 2000));
+    this_thread::sleep_for(val);
 
-void prepareCheeseFondue(){}
+    //unpile kitchen assistants from the kitchenAssistantWaitingForHelps counter
+    if (nbKitchenAssistantsWaitingForHelps >= 3)
+    {
+        nbKitchenAssistantsWaitingForHelps = nbKitchenAssistantsWaitingForHelps - 3;
+        for(int j = 0; j < 3; ++j){
+            kitchenAssistantLockerMutex[orderedQueue[j]].unlock();
+        }
+        orderedQueue.erase(orderedQueue.begin(), orderedQueue.begin()+3);
+    }
+    else
+        cout << "erreur au niveaux du nombre d'assistant demandant de l'aide"<< endl;
+
+    kitchenAssistantsMutex.unlock();
+}
+
+void giveCheese(){
+    // must get called by cheese hunter when 3 stars swiss chef calls prepareCheeseFondue
+}
+
+void prepareCheeseFondue(){
+    cout << "la fondue est prête" << endl;
+    kitchenAssistantsMutex.lock();
+}
 
 void commingBackFromCheeseHunt(){
     cheeseHuntersMutex.lock();
@@ -62,10 +88,70 @@ void kitchenAssistant(unsigned i){
     }
 }
 
-void threeStarsSwissChef(){}
+void threeStarsSwissChef(){
+    while (true){
+        threeStarsSwissChefMutex.lock();
+        if(nbCheeseHuntersThatCameBack == 9)
+        {
+            prepareCheeseFondue();
+            // signal to cheese hunter
+            break;
+        }else if(nbKitchenAssistantsWaitingForHelps >= 3){
+            //cout << "je suis le chef de cuissine et il y a " << nbKitchenAssistantsWaitingForHelps << " qui attende mon aide" << endl;
+            helpKitchenAssistants();
+        }
+        threeStarsSwissChefMutex.unlock();
+    }
+}
 
 int main()
 {
+    thread threeStarsSwissChefThread (threeStarsSwissChef);
 
+    thread cheeseHunter1Thread(cheeseHunter, 1);
+    thread cheeseHunter2Thread(cheeseHunter, 2);
+    thread cheeseHunter3Thread(cheeseHunter, 3);
+    thread cheeseHunter4Thread(cheeseHunter, 4);
+    thread cheeseHunter5Thread(cheeseHunter, 5);
+    thread cheeseHunter6Thread(cheeseHunter, 6);
+    thread cheeseHunter7Thread(cheeseHunter, 7);
+    thread cheeseHunter8Thread(cheeseHunter, 8);
+    thread cheeseHunter9Thread(cheeseHunter, 9);
+
+
+    thread kitchenAssistant1Thread (kitchenAssistant, 0);
+    thread kitchenAssistant2Thread (kitchenAssistant, 1);
+    thread kitchenAssistant3Thread (kitchenAssistant, 2);
+    thread kitchenAssistant4Thread (kitchenAssistant, 3);
+    thread kitchenAssistant5Thread (kitchenAssistant, 4);
+    thread kitchenAssistant6Thread (kitchenAssistant, 5);
+    thread kitchenAssistant7Thread (kitchenAssistant, 6);
+    thread kitchenAssistant8Thread (kitchenAssistant, 7);
+    thread kitchenAssistant9Thread (kitchenAssistant, 8);
+    thread kitchenAssistant10Thread (kitchenAssistant, 9);
+
+
+    threeStarsSwissChefThread.join();
+
+    cheeseHunter1Thread.join();
+    cheeseHunter2Thread.join();
+    cheeseHunter3Thread.join();
+    cheeseHunter4Thread.join();
+    cheeseHunter5Thread.join();
+    cheeseHunter6Thread.join();
+    cheeseHunter7Thread.join();
+    cheeseHunter8Thread.join();
+    cheeseHunter9Thread.join();
+
+    kitchenAssistant1Thread.join();
+    kitchenAssistant2Thread.join();
+    kitchenAssistant3Thread.join();
+    kitchenAssistant4Thread.join();
+    kitchenAssistant5Thread.join();
+    kitchenAssistant6Thread.join();
+    kitchenAssistant7Thread.join();
+    kitchenAssistant8Thread.join();
+    kitchenAssistant9Thread.join();
+    kitchenAssistant10Thread.join();
     return 0;
 }
